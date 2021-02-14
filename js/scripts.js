ea5815d15800145456b9d8b0137f76a65f34066d
Saoud <saoud@saoud.dev>
2021-02-14 14:29:08 -0800

    adding attachContactListeners to scripts.js
    Co-authored-by: Parker Benoit <parker.benoit44@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -39,6 +39,28 @@ function AddressBook() {
   Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
   }
-//   Contact.prototype.update = function () {
-
-//   }
+  
+  // User Interface Logic ---------
+  let addressBook = new AddressBook();
+  
+  function displayContactDetails(addressBookToDisplay) {
+    let contactsList = $("ul#contacts");
+    let htmlForContactInfo = "";
+    Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+      const contact = addressBookToDisplay.findContact(key);
+      htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+    });
+    contactsList.html(htmlForContactInfo);
+  };
+  
+  $(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+      const inputtedFirstName = $("input#new-first-name").val();
+      const inputtedLastName = $("input#new-last-name").val();
+      const inputtedPhoneNumber = $("input#new-phone-number").val();
+      let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+      addressBook.addContact(newContact);
+      displayContactDetails(addressBook);
+    });
+  });
